3cd9dbbfba93924a8bbeb6297c0b7874d018448c
AnimatorMax <maxcohn@live.com>
2024-06-14 19:39:43 -0400

    Added useless button

--- bot/bot.py
+++ bot/bot.py
@@ -7,10 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-class CustomClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
+client = discord.Client(intents=discord.Intents.default())
 
-client = CustomClient(intents=discord.Intents.default())
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if str(message.channel) == 'bot-stuff':
+        print("BEEP BOOP MESSAGE DETECTED")
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(label='test', custom_id='test-id'))
+        await message.channel.send("TEST BUTTON", view=view)
+    
 
 client.run(TOKEN)
