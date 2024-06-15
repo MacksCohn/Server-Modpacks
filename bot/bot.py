a39149d6115a940d32b26001de3f2a7b602cd1b2
AnimatorMax <maxcohn@live.com>
2024-06-14 21:05:22 -0400

    Working simple button with callback

--- bot/bot.py
+++ bot/bot.py
@@ -8,10 +8,17 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client(intents=discord.Intents.default())
+server = None
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+
+async def on_status_button(interaction : discord.Interaction):
+    status_string : str = await pull_status()
+    channel = discord.utils.get(client.get_all_channels(), name='bot-stuff')
+    print (str(channel))
 
 @client.event
 async def on_message(message):
@@ -20,8 +27,13 @@ async def on_message(message):
     if str(message.channel) == 'bot-stuff':
         print("BEEP BOOP MESSAGE DETECTED")
         view = discord.ui.View()
-        view.add_item(discord.ui.Button(label='test', custom_id='test-id'))
+        test_button = discord.ui.Button(label='Status', custom_id='status-id', style=discord.ButtonStyle.blurple)
+        test_button.callback = on_status_button
+        view.add_item(test_button)
         await message.channel.send("TEST BUTTON", view=view)
+        
+async def pull_status():
+    pass
     
 
 client.run(TOKEN)
