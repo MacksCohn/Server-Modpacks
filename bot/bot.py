307fcbaabbba6f0662a3e5e87726016d05badb96
AnimatorMax <maxcohn@live.com>
2024-06-16 10:27:06 -0400

    Responses work for start server

--- bot/bot.py
+++ bot/bot.py
@@ -110,6 +110,7 @@ async def on_start_button(interaction : discord.Interaction):
     if interaction.user.id == 463869439255904257 or interaction.user.id == 767538898737037362:
         os.chdir(SERVER_DIRECTORY_PATH)
         os.system(BATCH_PATH)
+        interaction.response.is_done=True
         print(BATCH_PATH)
 
 # Sync functions
