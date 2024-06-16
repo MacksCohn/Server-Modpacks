f058a0d307c8d7823e8f626def20865ddf1b2e9a
AnimatorMax <maxcohn@live.com>
2024-06-15 22:58:00 -0400

    Confused about backend

--- bot/bot.py
+++ bot/bot.py
@@ -45,7 +45,7 @@ async def on_message(message):
             print("Sending command: " + str(command))
             server_command(command)
             time.sleep(0.5)
-            screenshot = ImageGrab.grab(backend='scrot')
+            screenshot = ImageGrab.grab()
             screenshot.save('screenshot.png')
             screenshot.close()
             await message.channel.send(file=discord.File('screenshot.png'))
